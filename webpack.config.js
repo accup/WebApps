2185a22e8dff1c3c2ecd53eed4424bca5951181a
Accup <32096121+accup@users.noreply.github.com>
2020-03-16 17:43:32 +0900

    [update] remove html andd css loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,24 +8,5 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve('..', __dirname, 'docs')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.html$/i,
-				loader: 'html-loader'
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					'style-loader',
-					// Translates CSS into CommonJS
-					'css-loader',
-					// Compiles Sass to CSS
-					'sass-loader'
-				]
-			}
-		]
 	}
 }
