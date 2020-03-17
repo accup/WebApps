6b1f862ff7299ccec03a32dbaaae8f671ab5f92a
Accup <32096121+accup@users.noreply.github.com>
2020-03-18 00:00:30 +0900

    [add] Nuxt設定

---
+++ nuxt.config.js
@@ -0,0 +1,18 @@
+export default {
+	mode: 'spa',
+	head: {
+		/**
+		 * タイトルの変換処理
+		 * @param {string} title 元のタイトル
+		 */
+		titleTemplate (title) {
+			const rootTitle = 'WebApps';
+			return title ? `${title} - ${rootTitle}` : rootTitle;
+		},
+		/** metaタグ */
+		meta: [
+			{ charset: 'UTF-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+		]
+	}
+};
