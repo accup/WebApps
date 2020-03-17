addc2de4d4af4a40ad02f0e1a7f797b92f5e5459
Accup <32096121+accup@users.noreply.github.com>
2020-03-18 01:25:35 +0900

    [add, remove] translate to vue

---
+++ pages/Lambda/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<main id="main-area">
+		<textarea id="input-area"></textarea>
+		<div id="output-area"></div>
+	</main>
+</template>
+
+<script>
+export default {
+	head: {
+		bodyAttrs: {
+			id: 'main-wrapper'
+		},
+		script: [
+			{ src: './Lambda.js' },
+			{ src: './script.js' }
+		]
+	}
+}
+</script>
+
+<style lang="sass" scoped>
+@import "~/assets/Lambda/style.scss";
+</style>
---
+++ pages/Trees/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<canvas class="resizable" id="back"></canvas>
+		<canvas class="resizable" id="workspace">HTML5 Canvas 対応のブラウザで開いてください。</canvas>
+	</div>
+</template>
+
+<script>
+export default {
+	head: {
+		script: [
+			{ src: './util.js' },
+			{ src: './Trees.js' },
+			{ src: './main.js' }
+		]
+	}
+}
+</script>
+
+<style lang="sass" scoped>
+@import "~/assets/Trees/main.scss";
+</style>
