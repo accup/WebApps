6bff03710db62c9f1a44b1b141184abd14cca36b
Accup <32096121+accup@users.noreply.github.com>
2020-04-04 17:54:08 +0900

    [add] BeatsPerMinute application

---
+++ assets/BeatsPerMinute/BeatFilter.ts
@@ -0,0 +1,124 @@
+
+
+/** ビート推定器のインターフェース */
+interface IBeatEstimator {
+	/** BPSビンの個数 */
+	readonly numOfBpsBins: number;
+	/** リセット状態 */
+	readonly isReset : boolean;
+	/** 推定されたBPS */
+	readonly estimatedBps : number;
+	/** 推定された基準時刻 */
+	readonly estimatedAnchorTimeInSeconds : number;
+	
+	/** 新しいビートを入力する */
+	beat(now_time_in_seconds : number): void;
+	
+	/** 推定器の状態をリセットする（推定をやり直す） */
+	reset(): void;
+}
+
+
+/** ビート推定器の実装 */
+export class BeatEstimator implements IBeatEstimator {
+	/** BPS */
+	#bpsBins : Float64Array;
+	/** 最新のビート回転子の状態 */
+	#beatSpinXs : Float64Array;
+	/** 最新のビート回転子の状態 */
+	#beatSpinYs : Float64Array;
+	/** ローパスフィルタの係数（新しい値の反映のされにくさ） */
+	#lowPassAlpha : number;
+	/** リセット状態 */
+	#isReset : boolean = true;
+	/** 推定されたBPS */
+	#estimatedBps : number = 0;
+	/** 推定された基準時刻 */
+	#estimatedAnchorTimeInSeconds : number = 0;
+	
+	constructor(
+		beginBps : number,
+		endBps : number,
+		numOfBpsBins : number,
+		endPoint : boolean = false,
+		lowPassAlpha : number = 0.95
+	) {
+		this.#lowPassAlpha = lowPassAlpha;
+		
+		this.#bpsBins = new Float64Array(numOfBpsBins);
+		let endIndex = endPoint ? (this.#bpsBins.length - 1) : this.#bpsBins.length;
+		for (let index = 0; index < this.#bpsBins.length; index++) {
+			const bps = ((endIndex - index) * beginBps + index * endBps) / endIndex;
+			this.#bpsBins[index] = bps;
+		}
+		
+		this.#beatSpinXs = new Float64Array(this.#bpsBins.length);
+		this.#beatSpinYs = new Float64Array(this.#bpsBins.length);
+	}
+	
+	get numOfBpsBins() {
+		return this.#bpsBins.length;
+	}
+	
+	get isReset() {
+		return this.#isReset;
+	}
+	
+	get estimatedBps() {
+		return this.#estimatedBps;
+	}
+	
+	get estimatedAnchorTimeInSeconds() {
+		return this.#estimatedAnchorTimeInSeconds;
+	}
+	
+	beat(now_time_in_seconds : number) {;
+		const now_time_in_radians = 2 * Math.PI * now_time_in_seconds;
+		
+		if (this.#isReset) {
+			this.#isReset = false;
+			for (let index = 0; index < this.#bpsBins.length; index++) {
+				const bps = this.#bpsBins[index];
+				const now_time_in_n_radians = now_time_in_radians * bps;
+				this.#beatSpinXs[index] = Math.cos(now_time_in_n_radians);
+				this.#beatSpinYs[index] = Math.sin(now_time_in_n_radians);
+			}
+		} else {
+			for (let index = 0; index < this.#bpsBins.length; index++) {
+				const bps = this.#bpsBins[index];
+				const now_time_in_n_radians = now_time_in_radians * bps;
+				this.#beatSpinXs[index] = (
+					this.#lowPassAlpha * this.#beatSpinXs[index]
+					+ (1 - this.#lowPassAlpha) * Math.cos(now_time_in_n_radians)
+				);
+				this.#beatSpinYs[index] = (
+					this.#lowPassAlpha * this.#beatSpinYs[index]
+					+ (1 - this.#lowPassAlpha) * Math.sin(now_time_in_n_radians)
+				);
+			}
+		}
+		
+		let bestScore = -Infinity;
+		let bestBps = 0.0;
+		let bestAnchorTimeInSeconds = 0.0;
+		for (let index = 0; index < this.#bpsBins.length; index++) {
+			const score = this.#beatSpinYs[index] ** 2 + this.#beatSpinXs[index] ** 2;
+			if (bestScore < score) {
+				const bps = this.#bpsBins[index];
+				const time_in_n_radians = Math.atan2(this.#beatSpinYs[index], this.#beatSpinXs[index]);
+				const time_in_seconds = time_in_n_radians / (2 * Math.PI * bps);
+				
+				bestScore = score;
+				bestBps = bps;
+				bestAnchorTimeInSeconds = time_in_seconds;
+			}
+		}
+		
+		this.#estimatedBps = bestBps;
+		this.#estimatedAnchorTimeInSeconds = bestAnchorTimeInSeconds;
+	}
+	
+	reset() {
+		this.#isReset = true;
+	}
+}
---
+++ pages/BeatsPerMinute/index.vue
@@ -0,0 +1,107 @@
+<template>
+	<div id='main'>
+		<div id='bpmViewer'>
+			{{ bpm }}
+			<span class='small-caps'>BPM</span>
+		</div>
+		<div id='controller'>
+			<button id='beatButton' @click="beat">
+				{{ beatButtonText }}
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import { BeatEstimator } from '@/assets/BeatsPerMinute/BeatFilter.ts';
+
+
+const beatEstimator = new BeatEstimator(
+	60.0 / 60,
+	240.0 / 60,
+	180
+);
+let resetTimerId = null;
+
+
+export default {
+	data: () => ({
+		bpm: 120,
+		beatCount: 0
+	}),
+	computed: {
+		beatButtonText() {
+			if (this.beatCount == 0) {
+				return 'Beat';
+			} else {
+				return `${this.beatCount}`;
+			}
+		}
+	},
+	methods: {
+		beat() {
+			if (this.beatCount == 0) {
+				beatEstimator.reset();
+			}
+			
+			beatEstimator.beat(performance.now() / 1000.0);
+			this.bpm = beatEstimator.estimatedBps * 60;
+			++this.beatCount;
+			
+			if (resetTimerId != null) {
+				clearTimeout(resetTimerId);
+			}
+			resetTimerId = setTimeout(this.resetBeatEstimator, 2000);
+		},
+		resetBeatEstimator() {
+			resetTimerId = null;
+			beatEstimator.reset();
+			this.beatCount = 0;
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+#main {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	
+	background-color: #080010;
+	
+	display: grid;
+	grid-template-rows: 1fr 1fr 2fr;
+	
+	#bpmViewer {
+		grid-row: 2;
+		
+		margin: auto;
+		
+		color: white;
+		font-size: 2.4em;
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+		text-align: center;
+	}
+	
+	#controller {
+		grid-row: 3;
+		
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		
+		#beatButton {
+			flex: 0 0 10em;
+			width: 10em;
+		}
+	}
+}
+
+.small-caps {
+	font-variant: small-caps;
+}
+</style>
