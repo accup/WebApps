f066ecc04157f7cbf19067bd0b5c704d21c5179e
Accup <32096121+accup@users.noreply.github.com>
2020-06-02 00:11:32 +0900

    [update] ただ表示するだけのレイアウト

--- apps/Sigval/layouts/default.vue
+++ apps/Sigval/layouts/default.vue
@@ -1,11 +1,5 @@
 <template>
-  <v-app>
-    <v-content>
-      <transition name="page">
-        <nuxt />
-      </transition>
-    </v-content>
-  </v-app>
+  <nuxt />
 </template>
 
 <script>
@@ -18,19 +12,4 @@ export default {
 html {
   overflow-y: auto !important;
 }
-
-// Fix the `v-bottom-navigation` and `v-btn`s issue on treeshake mode. (GitHub:vuetifyjs/vuetify #8067)
-.v-item-group.v-bottom-navigation .v-btn.v-size--default {
-  height: inherit;
-}
-
-.page {
-  &-enter-active, &-leave-active {
-    transition: opacity .5s;
-  }
-  &-enter, &-leave-to {
-    opacity: 0;
-  }
-}
-
 </style>
